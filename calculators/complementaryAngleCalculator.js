cfa86ca80966fcc5790663df7a142f918eb58208
AbigailPape <34281793+AbigailPape@users.noreply.github.com>
2018-01-05 15:01:10 -0600

    Update complementaryAngleCalculator.js
    uncommented if else

--- calculators/complementaryAngleCalculator.js
+++ calculators/complementaryAngleCalculator.js
@@ -1,8 +1,8 @@
 function complementaryAngleCalculator() {
   var origAngle= prompt("What angle measure, in degrees, would you like to find the complement of?\nInput Format: A Number\nExample: 54");
-  /*if (typeof origAngle === "number" &&& origAngle >= 0 && origAngle <= 90) {
+  if (typeof origAngle === "number" &&& origAngle >= 0 && origAngle <= 90) {
     alert("Under construction");
   } else {
     alert("\'" + origAngle + "\'" + "is not valid input.\nBe sure your input is just a number and is between or equal to 0&deg; and 90&deg;. Otherwise, its complement does not exist.");
-  }*/
+  }
 };
