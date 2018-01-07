4fa75b85fb71fe86416e4dad887e295091387456
AbigailPape <34281793+AbigailPape@users.noreply.github.com>
2018-01-07 11:00:53 -0600

    Update complementaryAngleCalculator.js
    quick \n

--- calculators/complementaryAngleCalculator.js
+++ calculators/complementaryAngleCalculator.js
@@ -3,7 +3,7 @@ function complementaryAngleCalculator() {
   if (origAngle != null) {
     if (origAngle >= 0 && origAngle <= 90) {
       var complement = 90 - origAngle;
-      alert("To find the complement of " + origAngle + " degree(s), we must subtract the angle's measure from 90 degrees.\nWork:\n90 - " + origAngle + " = " + complement + "\n\nTo see the answer, click \'OK\'.");
+      alert("To find the complement of " + origAngle + " degree(s), we must subtract the angle's measure from 90 degrees.\n\nWork:\n90 - " + origAngle + " = " + complement + "\n\nTo see the answer, click \'OK\'.");
       alert("The complement of " + origAngle + " degree(s) is " + complement + " degree(s).");
     } else {
       alert("\'" + origAngle + "\'" + " is not valid input.\nBe sure your input is a number (in degrees) and is between or equal to 0 and/or 90. Otherwise, its complement does not exist.");
