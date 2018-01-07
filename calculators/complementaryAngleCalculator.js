2f0a22a5335f681e336eeb0a8ca4ceedae037d11
AbigailPape <34281793+AbigailPape@users.noreply.github.com>
2018-01-07 11:05:57 -0600

    Update complementaryAngleCalculator.js
    another quick \n

--- calculators/complementaryAngleCalculator.js
+++ calculators/complementaryAngleCalculator.js
@@ -1,5 +1,5 @@
 function complementaryAngleCalculator() {
-  var origAngle= prompt("What angle measure, in degrees, would you like to find the complement of?\nInput Format: A Number\nExamples: 54, 8, 73.96");
+  var origAngle= prompt("What angle measure, in degrees, would you like to find the complement of?\n\nInput Format: A Number\nExamples: 54, 8, 73.96");
   if (origAngle != null) {
     if (origAngle >= 0 && origAngle <= 90) {
       var complement = 90 - origAngle;
