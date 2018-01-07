a9d4e1aea9146b373dc22ee2c4da49d79e8772cb
AbigailPape <34281793+AbigailPape@users.noreply.github.com>
2018-01-07 10:47:05 -0600

    Update complementaryAngleCalculator.js
    did the if of the if else statement

--- calculators/complementaryAngleCalculator.js
+++ calculators/complementaryAngleCalculator.js
@@ -2,7 +2,9 @@ function complementaryAngleCalculator() {
   var origAngle= prompt("What angle measure, in degrees, would you like to find the complement of?\nInput Format: A Number\nExamples: 54, 8, 73.96");
   if (origAngle != null) {
     if (origAngle >= 0 && origAngle <= 90) {
-      alert("Under construction");
+      var complement = 90 - origAngle;
+      alert("To find the complement of " + origAngle + " degree(s), we must subtract the angle's measure from 90 degrees.\nWork:\n90 - " + origAngle + " = " + complement + "\n\nTo see the answer, click \'OK\'.");
+      alert("The complement of " + origAngle + " degrees is " + complement + " degrees.");
     } else {
       alert("\'" + origAngle + "\'" + " is not valid input.\nBe sure your input is a number (in degrees) and is between or equal to 0 and/or 90. Otherwise, its complement does not exist.");
     }
